c317124c05bd42a909b5e6ff995149848fde890e
PoShawn <100253629+PoShawn@users.noreply.github.com>
2022-04-11 17:24:58 +0800

    Update Parobola Report.py

--- Parobola Report.py
+++ Parobola Report.py
@@ -7,9 +7,10 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import tensorflow as tf
-import keras
+
 
 # Get Data
+# return Vector 分別為[初速,角度,最大垂直距離,最大水平距離]
 def Parabola(V0,theta):
     g = 9.8
     Vx = V0 * math.cos( theta / 180 * math.pi ) # Vx 水平方向速度
@@ -20,50 +21,38 @@ def Parabola(V0,theta):
     Vector = [V0,theta,H,R]
     return Vector
 
+# 總資料集為Dataset，利用迴圈輸入10000組資料
+# 初速為1到100隨機取數，角度為1到89隨機取數
 Dataset = np.zeros((10000,4))
 for i in range(0,10000,1):
     Dataset[i,:] = Parabola(random.randint(1,100),random.randint(1,89))
-print(Dataset)
-# Test Data
-# Improve
-# 分別帶三個 Activation Function試試看 (ReLU)
 
+    
 # Clean, Prepare & Manipulate Data
+# 第ㄧ欄與第二欄為輸入層，第三欄與第四欄為輸出層
+# 將10000筆分成8000筆訓練集與2000筆測試集
 X_train = Dataset[:8000,:2]
 Y_train = Dataset[:8000,2:]
 X_test = Dataset[8000:,:2]
 Y_test = Dataset[8000:,2:]
-print(np.shape(X_train))
-print(X_train)
 
 # Train Model
-from keras.layers import Dense
-model = keras.Sequential()
-model.add(Dense(units = 10,
-                input_shape = (2,),
-                kernel_initializer = 'normal',
-                activation = 'relu'))
-model.add(Dense(units = 10,
-                kernel_initializer = 'normal',
-                activation = 'relu'))
-model.add(Dense(units = 2,
-                kernel_initializer = 'normal',
-                activation = 'softmax'))
+# Model共有輸入層、Hidden layer 兩層個各10個神經元，及輸出層
+model = tf.keras.models.Sequential([
+        tf.keras.layers.Dense(units = 10 , activation = tf.nn.relu , input_dim = 2) ,
+        tf.keras.layers.Dense(units = 10 , activation = tf.nn.relu),
+        tf.keras.layers.Dense(units = 2 , activation = tf.nn.softmax)
+])
 model.compile(optimizer = 'adam',
               loss = 'mean_absolute_error',
               metrics = ['accuracy'])
 model.summary()
+model.fit(X_train , Y_train , epochs = 500 , batch_size = 1000)
 
-model.fit(X_train , Y_train , epochs = 500 , batch_size = 100)
-
-#
-score = model.evaluate(X_test , Y_test , batch_size = 100)
+score = model.evaluate(X_test , Y_test , batch_size = 1000)
 print('score:',score)
 
-#
 Y_predict = model.predict(X_test)
-print(Y_test)
-print(Y_predict)
 
 plt.scatter(Y_test[:,0],Y_predict[:,0])
 plt.show
