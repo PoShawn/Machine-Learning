bf47d22530a4f5a0b4392279560acfdbf08b0259
PoShawn <100253629+PoShawn@users.noreply.github.com>
2022-04-19 13:52:35 +0800

    Update Parobola Report.py

--- Parobola Report.py
+++ Parobola Report.py
@@ -1,12 +1,14 @@
 %matplotlib inline
 
-#導入Library
+#import Library
 import math
 import random
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
 import tensorflow as tf
+from scipy import stats
 
 
 # Get Data
@@ -23,39 +25,115 @@ def Parabola(V0,theta):
 
 # 總資料集為Dataset，利用迴圈輸入10000組資料
 # 初速為1到100隨機取數，角度為1到89隨機取數
+
 Dataset = np.zeros((10000,4))
 for i in range(0,10000,1):
-    Dataset[i,:] = Parabola(random.randint(1,100),random.randint(1,89))
+    Dataset[i,:] = Parabola(random.randint(0,100),random.randint(1,89))
 
-    
+np.save('Dataset.npy',Dataset)
+Dataset = np.load('Dataset.npy')
+np.savetxt('Dataset.txt',Dataset)
+
 # Clean, Prepare & Manipulate Data
 # 第ㄧ欄與第二欄為輸入層，第三欄與第四欄為輸出層
 # 將10000筆分成8000筆訓練集與2000筆測試集
 X_train = Dataset[:8000,:2]
 Y_train = Dataset[:8000,2:]
-X_test = Dataset[8000:,:2]
-Y_test = Dataset[8000:,2:]
+X_test  = Dataset[8000:,:2]
+Y_test  = Dataset[8000:,2:]
+
+Dataset_df = pd.DataFrame(Dataset,columns = ['V0','θ','H','R'])
+sns.pairplot(Dataset_df)
 
 # Train Model
 # Model共有輸入層、Hidden layer 兩層個各10個神經元，及輸出層
+
 model = tf.keras.models.Sequential([
         tf.keras.layers.Dense(units = 10 , activation = tf.nn.relu , input_dim = 2) ,
         tf.keras.layers.Dense(units = 10 , activation = tf.nn.relu),
-        tf.keras.layers.Dense(units = 2 , activation = tf.nn.softmax)
+        tf.keras.layers.Dense(units = 10 , activation = tf.nn.relu),
+        tf.keras.layers.Dense(units = 10 , activation = tf.nn.relu),
+        tf.keras.layers.Dense(units = 10 , activation = tf.nn.relu),
+        tf.keras.layers.Dense(units = 2)
 ])
-model.compile(optimizer = 'adam',
-              loss = 'mean_absolute_error',
-              metrics = ['accuracy'])
-model.summary()
-model.fit(X_train , Y_train , epochs = 500 , batch_size = 1000)
 
-score = model.evaluate(X_test , Y_test , batch_size = 1000)
+model.compile(optimizer = 'adam',
+              loss = 'mse',
+              metrics = ['accuracy','mae','mse'])
+model.summary()
+history = model.fit(X_train , Y_train , epochs = 500 , batch_size = 500 , validation_split = 0.2 , verbose = 0)
+
+score = model.evaluate(X_test , Y_test , batch_size = 500 , verbose = 0)
 print('score:',score)
 
 Y_predict = model.predict(X_test)
 
-plt.scatter(Y_test[:,0],Y_predict[:,0])
-plt.show
+plt.figure(dpi = 150)
+plt.title('Train & Validation Accuracy')
+plt.plot(history.history['accuracy'],label = 'Train_Accuracy')
+plt.plot(history.history['val_accuracy'],label = 'Validation_Accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.ylim(0,1)
+plt.legend(loc = 'lower right')
+plt.savefig('Accuracy.png', dpi = 150)
 
-plt.scatter(Y_test[:,1],Y_predict[:,1])
-plt.show
+plt.figure(dpi = 150)
+plt.title('Train & Validation MAE')
+plt.plot(history.history['mae'],label = 'Train')
+plt.plot(history.history['val_mae'],label = 'Validation')
+plt.xlabel('Epoch')
+plt.ylabel('Mean Abs Error')
+plt.legend()
+plt.savefig('MAE.png', dpi = 150)
+
+plt.figure(dpi = 150)
+plt.title('Train & Validation MSE')
+plt.plot(history.history['mse'],label = 'Train')
+plt.plot(history.history['val_mse'],label = 'Validation')
+plt.xlabel('Epoch')
+plt.ylabel('Mean Square Error')
+plt.legend()
+plt.savefig('MSE', dpi = 150)
+
+Y_test_reshape = np.reshape(Y_test[:,0],(2000,))
+Y_predict_reshape = np.reshape(Y_predict[:,0],(2000,))
+
+plt.figure(dpi = 150)
+plt.scatter(Y_test[:,0],Y_predict[:,0],marker = '+')
+plt.plot([0,2000],[0,2000],'black',linewidth = 3)
+plt.xlabel('Real H (m)')
+plt.ylabel('Predict H (m)')
+plt.savefig('H.png', dpi = 150)
+
+R_Pearson = stats.pearsonr(Y_test_reshape , Y_predict_reshape)
+print('R_Pearson = ', R_Pearson[0])
+
+Y_test_reshape = np.reshape(Y_test[:,1],(2000,))
+Y_predict_reshape = np.reshape(Y_predict[:,1],(2000,))
+
+plt.figure(dpi = 150)
+plt.scatter(Y_test[:,1],Y_predict[:,1],marker = '+')
+plt.plot([0,1000],[0,1000],'black',linewidth = 3)
+plt.xlabel('Real R (m)')
+plt.ylabel('Predict R (m)')
+plt.savefig('R.png', dpi = 150)
+
+R_Pearson = stats.pearsonr(Y_test_reshape , Y_predict_reshape)
+print('R_Pearson = ', R_Pearson[0])
+
+error = Y_predict[:,0] - Y_test[:,0]
+plt.figure(dpi = 150)
+plt.hist(error, bins = 100)
+plt.xlabel("Prediction Error H")
+plt.ylabel("Count")
+plt.xlim(-50,50)
+plt.savefig('Error H.png', dpi = 150)
+
+error = Y_predict[:,1] - Y_test[:,1]
+plt.figure(dpi = 150)
+plt.hist(error, bins = 100)
+plt.xlabel("Prediction Error R")
+plt.ylabel("Count")
+plt.xlim(-50,50)
+plt.savefig('Error R.png', dpi = 150)
